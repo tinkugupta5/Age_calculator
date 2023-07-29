c840b8984f7fcd05c22f7b031bdf8e52d9ce0c72
tinkugupta5 <tinku.gupta4@gmail.com>
2023-07-29 22:11:44 +0530

    adding

--- src/componets/AgeCalculator.js
+++ src/componets/AgeCalculator.js
@@ -21,28 +21,33 @@ const AgeCalculator = () => {
     const resetCalculator = () => {
         setBirthdate('');
         setAge(0);
-    };
+      };
 
     return (
         <>
             <div className='Container'>
-                <h2 className='heading container_title'>Age Calculator</h2>
-                <p className=' para container_title'>The Age Calculator can determine the age or interval between two dates. The calculated age will be displayed in years,</p>
-                <div className='Container_middle'>
+             <h2 className='heading container_title'>Age Calculator</h2>
+             <p className=' para container_title'>The Age Calculator can determine the age or interval between two dates. The calculated age will be displayed in years,</p>
+             <div className='Container_middle'>
                     <div className='right'>
                         <h4>Date of Birth</h4>
                         <input className='date' type="date" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
-                          <div className='button_div'>
-                            <button className='button-65' onClick={calculateAge}>Calculate Age</button>
-                            <button className="button-65" onClick={resetCalculator}> Reset </button> </div>
+                        <div className='button_div'>
+                        <button className='button-65' onClick={calculateAge}>Calculate Age</button>
+                        <button className="button-65" onClick={resetCalculator}>
+                Reset
+              </button>
+                       
+                        </div>
                     </div>
+                    
                     <div className='left'>
                         <div className='Container_middle_para'>
                             <h1>Your Age is</h1>
                         </div>
                         <h1 className='age_heading'>{age > 0 ? ` ${age}` : ''}</h1>
                     </div>
-                </div>
+             </div>
             </div>
         </>
     );
